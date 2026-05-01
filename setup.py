7c7cf643190305ab0c54da4e9b96214b057442dc
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:34:46 -0700

    Add explicit build-system defs, stub setup.py for setuptools <61.0.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: finnix-mirrors-website
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/finnix/finnix-mirrors-website
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
